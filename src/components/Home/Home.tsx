6bb68b4c891acbf0e67684df58f35b69f1493ac3
Indunil Ranganath <indunil@travelai.lk>
2024-08-13 01:59:35 +0530

    update

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -203,33 +203,16 @@ function Home() {
           <h2 className="text-2xl font-bold mb-6">How can we help?</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-12">
             <div className="bg-gray-300 p-4 rounded shadow hover:bg-gray-400 transition-all cursor-pointer bg-img text-white">
-              <h3 className="text-lg font-semibold mb-2">Blog</h3>
-              <p className="text-sm mb-4">
-                Efficient Delivery: Offering prompt and reliable delivery
-                services to ensure your materials arrive on time.
-              </p>
-              <div className="flex justify-between items-center">
-                <hr className="border-black w-full" />
-                <span className="ml-2 cursor-pointer">⌄</span>
-              </div>
-            </div>
-            <div className="bg-gray-300 p-4 rounded shadow hover:bg-gray-400 transition-all cursor-pointer bg-img text-white">
               <h3 className="text-lg font-semibold mb-2">
-                The Importance of Choosing the Right Brick: Insights from UK
-                Brick’s ANT Accredited Programme
+                Are Construction Material Shortages Here to Stay?
               </h3>
               <span className="flex flex-col">
-              <p className="text-sm mb-4 flex text-justify">
-                In the world of architecture, not all bricks are created equal.
-                UK Brick’s ANT-accredited course, "Designing with Brick,"
-                highlights the crucial role that brick selection plays in
-                architectural design. Architects are often faced with a vast
-                array of brick types, each offering unique qualities that can
-                dramatically influence a project’s success.
-              </p>
-              <p className="text-sm mb-4 flex text-justify">
-              From aesthetic appeal to durability, thermal insulation, and load-bearing capabilities, bricks vary widely in their characteristics. This diversity makes it essential for architects to carefully consider the specific needs of each project. The right brick can enhance not only the structural integrity of a building but also its overall visual appeal.
-              </p>
+                <p className="text-sm mb-4 flex text-justify">
+                The UK is currently experiencing a significant shortage of essential construction materials such as bricks, steel, adhesives, paints, and timber. While larger construction companies have managed to navigate these challenges, smaller businesses are struggling to keep up.
+                </p>
+                <p className="text-sm mb-4 flex text-justify">
+                Despite the recent trade deal between the UK and the EU, which addresses some issues, it fails to tackle one of the most pressing concerns for construction companies: the new UK Conformity Assessed (UKCA) certification. As of January 1, 2022, all European products used in UK construction must be UKCA certified.
+                </p>
               </span>
               <div className="flex justify-between items-center">
                 <hr className="border-black w-full" />
@@ -237,11 +220,43 @@ function Home() {
               </div>
             </div>
             <div className="bg-gray-300 p-4 rounded shadow hover:bg-gray-400 transition-all cursor-pointer bg-img text-white">
-              <h3 className="text-lg font-semibold mb-2">Blog</h3>
-              <p className="text-sm mb-4">
-                Professional Loading: Offering professional loading services to
-                ensure your materials are handled with care and precision.
-              </p>
+              <h3 className="text-lg font-semibold mb-2">
+                The Importance of Choosing the Right Brick: Insights from UK ANT
+                CPD Accredited Programme
+              </h3>
+              <span className="flex flex-col">
+                <p className="text-sm mb-4 flex text-justify">
+                  In the world of architecture, not all bricks are created
+                  equal. UK ANT CPD-accredited course, "Designing with Brick,"
+                  highlights the crucial role that brick selection plays in
+                  architectural design. Architects are often faced with a vast
+                  array of brick types, each offering unique qualities that can
+                  dramatically influence a project’s success.
+                </p>
+                <p className="text-sm mb-4 flex text-justify">
+                  From aesthetic appeal to durability, thermal insulation, and
+                  load-bearing capabilities, bricks vary widely in their
+                  characteristics. This diversity makes it essential for
+                  architects to carefully consider the specific needs of each
+                  project. The right brick can enhance not only the structural
+                  integrity of a building but also its overall visual appeal.
+                </p>
+              </span>
+              <div className="flex justify-between items-center">
+                <hr className="border-black w-full" />
+                <span className="ml-2 cursor-pointer">⌄</span>
+              </div>
+            </div>
+            <div className="bg-gray-300 p-4 rounded shadow hover:bg-gray-400 transition-all cursor-pointer bg-img text-white">
+              <h3 className="text-lg font-semibold mb-2">Navigating the Building Material Shortage: Challenges and Insights</h3>
+              <span className="flex flex-col">
+                <p className="text-sm mb-4 flex text-justify">
+                The construction industry is facing a significant challenge as a major shortage of building materials is causing delays and budget concerns across the board. For many, this has become a serious obstacle, especially for those working within tight timelines and budgets.
+                </p>
+                <p className="text-sm mb-4 flex text-justify">
+                The shortage has had a direct impact on the availability and cost of materials such as bricks. The Construction Products Association (CPA) predicted a rise in construction output by 12.9% this year and 5.2% next year. However, these optimistic forecasts are now under threat as the demand for materials far exceeds supply.
+                </p>
+              </span>
               <div className="flex justify-between items-center">
                 <hr className="border-black w-full" />
                 <span className="ml-2 cursor-pointer">⌄</span>
